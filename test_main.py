f08a4f68517f6f9692ec935d4a7df825a3eea4ac
Olzhas Yerezhepov <bubbles.gums69@gmail.com>
2023-04-08 22:45:59 +0500

    3-я Лабораторная, исправил ошибки с учетом flake8

--- test_main.py
+++ test_main.py
@@ -3,17 +3,20 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_read_main_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "This is neural network's main page"}
 
+
 def test_read_main_demo():
     response = client.get("/demo/")
     assert response.status_code == 200
 
+
 def test_read_main_predict():
-    response = client.post("/predict/", json={"text":"Hello world!"})
+    response = client.post("/predict/", json={"text": "Hello world!"})
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
     assert isinstance(response.json()["generated_text"], str)
